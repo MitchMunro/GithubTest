b217646f4c0c549198a697d8f8aa0844c8ade2b8
MitchMunro <mitchmunro@gmail.com>
2023-02-15 09:50:26 +1100

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Mitch Munro on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
